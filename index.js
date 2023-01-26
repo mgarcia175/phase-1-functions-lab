26fc4823375a1574f084da4ba415dd0584ff0375
Martin <martingarcia804@yahoo.com>
2023-01-25 19:58:25 -0600

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+let pickUp
+function distanceFromHqInBlocks(pickUp) {
+    if (pickUp > 42) {
+        return pickUp - 42
+    } else {
+        return 42 - pickUp
+    }
+}
+
+function distanceFromHqInFeet(pickUp) {
+    distanceFromHqInBlocks(pickUp);
+    return distanceFromHqInBlocks(pickUp) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return (start - destination) * 264
+    }
+    else {
+        return (destination - start) * 264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0
+    }
+    else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * .02
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return "cannot travel that far"
+    }
+}
